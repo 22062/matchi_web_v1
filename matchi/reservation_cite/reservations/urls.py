aa7efcb926a8977c49ad835be935589994e8bce3
aliywedad <aliysidahmedwedad@gmail.com>
2024-10-25 23:37:54 +0000

    changing the mobile Apis from views.py to mobile.py

--- matchi/reservation_cite/reservations/urls.py
+++ matchi/reservation_cite/reservations/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-from . import views
+from . import mobile,views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,47 +9,47 @@ router = DefaultRouter()
 router.register(r'terrains', views.TerrainViewSet, basename='terrain')
 
 urlpatterns = [
-   path('index', views.index , name='index'),
-#    path('ajouter_cite', views.ajouter_cite , name='ajouter_cite'),
-   path('gestion_client', views.gestion_client , name='gestion_client'),
-#    path('gestion_cite', views.gestion_cite , name='gestion_cite'),
-#    path('modifier_client/', views.modifier_client, name='modifier_client'),
-   path('supprimer_client/', views.supprimer_client, name='supprimer_client'),
-   path('ajouter_client/', views.ajouter_client, name='ajouter_client'),
-#    path('modifier_cite/<int:cite_id>/', views.modifier_cite, name='modifier_cite'),
-#    path('supprimer_cite/<int:cite_id>/', views.supprimer_cite, name='supprimer_cite'),
-#    path('', views.page_acceuil , name='page_acceuil'),
-    path('register/', views.register_client, name='register_client'),
-    path('login/', views.login_client, name='login_client'),
-    path('getPassword/', views.getPassword, name='getPassword'),
-    path('changePassword/', views.changePassword, name='changePassword'),
+   path('index', mobile.index , name='index'),
+#    path('ajouter_cite', mobile.ajouter_cite , name='ajouter_cite'),
+   path('gestion_client', mobile.gestion_client , name='gestion_client'),
+#    path('gestion_cite', mobile.gestion_cite , name='gestion_cite'),
+#    path('modifier_client/', mobile.modifier_client, name='modifier_client'),
+   path('supprimer_client/', mobile.supprimer_client, name='supprimer_client'),
+   path('ajouter_client/', mobile.ajouter_client, name='ajouter_client'),
+#    path('modifier_cite/<int:cite_id>/', mobile.modifier_cite, name='modifier_cite'),
+#    path('supprimer_cite/<int:cite_id>/', mobile.supprimer_cite, name='supprimer_cite'),
+#    path('', mobile.page_acceuil , name='page_acceuil'),
+    path('register/', mobile.register_client, name='register_client'),
+    path('login/', mobile.login_client, name='login_client'),
+    path('getPassword/', mobile.getPassword, name='getPassword'),
+    path('changePassword/', mobile.changePassword, name='changePassword'),
     
-    path('registerr/', views.ClientCreateView.as_view(), name='register_client'),
-    path('add_terrain/', views.TerrainsCreateView.as_view(), name='add_terrain'),
-    path('clients/', views.ClientListView.as_view(), name='list_clients'),
-    path('get_user_info/', views.get_user_info, name='get_user_info'),
-    path('get_terrain_info/<int:client_id>/', views.get_terrain_info, name='get_terrain_info'),
-    path('get_all_terrains/', views.get_all_terrains, name='get_all_terrains'),
+    path('registerr/', mobile.ClientCreateView.as_view(), name='register_client'),
+    path('add_terrain/', mobile.TerrainsCreateView.as_view(), name='add_terrain'),
+    path('clients/', mobile.ClientListView.as_view(), name='list_clients'),
+    path('get_user_info/', mobile.get_user_info, name='get_user_info'),
+    path('get_terrain_info/<int:client_id>/', mobile.get_terrain_info, name='get_terrain_info'),
+    path('get_all_terrains/', mobile.get_all_terrains, name='get_all_terrains'),
     path('', include(router.urls)),
-    path('heures-disponibles/<int:client_id>/<str:date>/', views.heures_disponibles, name='heures_disponibles'),
-    path('terrains/', views.TerrainsListView.as_view(), name='terrains-list'),
-    path('terrains/<int:terrain_id>/available-schedules/', views.terrain_heures_disponibles, name='available_schedules'),
-    path('joueurs/', views.JoueursListView.as_view(), name='joueurs-list'),
-    path('joueurs/<int:joueur_id>/', views.joueur_detail, name='joueur_details'),
-    path('faire_reservation/', views.faire_reservation, name='faire_reservation'),
-    path('annuler_reservation/<int:reservation_id>/', views.annuler_reservation, name='annuler_reservation'),
-    path('get_reservations/<int:joueur_id>/', views.get_reservations, name='get_reservations'),
+    path('heures-disponibles/<int:client_id>/<str:date>/', mobile.heures_disponibles, name='heures_disponibles'),
+    path('terrains/', mobile.TerrainsListView.as_view(), name='terrains-list'),
+    path('terrains/<int:terrain_id>/available-schedules/', mobile.terrain_heures_disponibles, name='available_schedules'),
+    path('joueurs/', mobile.JoueursListView.as_view(), name='joueurs-list'),
+    path('joueurs/<int:joueur_id>/', mobile.joueur_detail, name='joueur_details'),
+    path('faire_reservation/', mobile.faire_reservation, name='faire_reservation'),
+    path('annuler_reservation/<int:reservation_id>/', mobile.annuler_reservation, name='annuler_reservation'),
+    path('get_reservations/<int:joueur_id>/', mobile.get_reservations, name='get_reservations'),
 
-    path('joueurs/<int:player_id>/update/', views.update_player, name='update_player'),
-    path('joueurs/<int:player_id>/uploadProfileImage/', views.uploadProfileImage, name='uploadProfileImage'),
+    path('joueurs/<int:player_id>/update/', mobile.update_player, name='update_player'),
+    path('joueurs/<int:player_id>/uploadProfileImage/', mobile.uploadProfileImage, name='uploadProfileImage'),
     # /joueurs/$playerId/uploadProfileImage/
-    path('joueurs/register/', views.JoueurCreateView.as_view(), name='joueur-register'),
-    path('client/<int:client_id>/reservations/', views.client_reservations, name='client_reservations'),
-    path('add-indisponibilite/', views.AddIndisponibiliteView.as_view(), name='add_indisponibilite'),
-    path('wilayes/', views.WilayeList.as_view(), name='wilaye-list'),
-    path('moughataas/<int:code_wilaye>/', views.get_moughataas, name='get_moughataas'),
-    path('ActiveDesactive/<int:joueur_id>/', views.ActiveDesactive, name='ActiveDesactive'),
-    path('academies/', views.AcademieListCreateAPIView.as_view(), name='academie-list-create'),
+    path('joueurs/register/', mobile.JoueurCreateView.as_view(), name='joueur-register'),
+    path('client/<int:client_id>/reservations/', mobile.client_reservations, name='client_reservations'),
+    path('add-indisponibilite/', mobile.AddIndisponibiliteView.as_view(), name='add_indisponibilite'),
+    path('wilayes/', mobile.WilayeList.as_view(), name='wilaye-list'),
+    path('moughataas/<int:code_wilaye>/', mobile.get_moughataas, name='get_moughataas'),
+    path('ActiveDesactive/<int:joueur_id>/', mobile.ActiveDesactive, name='ActiveDesactive'),
+    path('academies/', mobile.AcademieListCreateAPIView.as_view(), name='academie-list-create'),
 
 
 
@@ -59,15 +59,15 @@ urlpatterns = [
 
     # **************************************************************************( mobile APIs)********************************************************************
 
-    path('AddPlayer/', views.add_player, name='AddPlayer'),
-    path("LoginPlayer/", views.login_joueur, name='LoginPlayer'),
-    path('update-token/', views.update_token, name='update_token'),
-    path('create_reservation_request/', views.create_reservation_request, name='create_reservation_request'),
-    path('client/<int:client_id>/demandes-reservation/', views.DemandeReservationClientView.as_view(), name='demandes-reservation-client'),
-    path('updateFCMToken_joueur/<int:joueur_id>/', views.update_fcm_token_joueur, name='update_fcm_token_joueur'),
-    path('reservations/<int:reservation_id>/', views.update_reservation_status, name='update_reservation_status'),
-    path('client/<int:client_id>/reservations_confirmees/', views.nombre_reservations_confirmees, name='reservations_confirmees'),
-    path('DemandsCount', views.DemandsCount, name='/DemandsCount'),
+    path('AddPlayer/', mobile.add_player, name='AddPlayer'),
+    path("LoginPlayer/", mobile.login_joueur, name='LoginPlayer'),
+    path('update-token/', mobile.update_token, name='update_token'),
+    path('create_reservation_request/', mobile.create_reservation_request, name='create_reservation_request'),
+    path('client/<int:client_id>/demandes-reservation/', mobile.DemandeReservationClientView.as_view(), name='demandes-reservation-client'),
+    path('updateFCMToken_joueur/<int:joueur_id>/', mobile.update_fcm_token_joueur, name='update_fcm_token_joueur'),
+    path('reservations/<int:reservation_id>/', mobile.update_reservation_status, name='update_reservation_status'),
+    path('client/<int:client_id>/reservations_confirmees/', mobile.nombre_reservations_confirmees, name='reservations_confirmees'),
+    path('DemandsCount', mobile.DemandsCount, name='/DemandsCount'),
     
 ]
 
